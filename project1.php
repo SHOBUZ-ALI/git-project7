8ae880ccf6629a41e2312865b66106d88340e4e7
SHOBUZ-ALI <sobuz41du@gmail.com>
2024-10-21 15:22:42 +0600

    add my project1.php file

---
+++ project1.php
@@ -0,0 +1,169 @@
+<?php
+$conn=new mysqli("localhost","root","","project1");
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>project1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+	<nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container">
+  
+   <a class="navbar-brand text-white" href="project1.php">project cabinet</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="project1.php">Home</a>
+        </li>
+        </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  </head>
+  <body>
+  
+  
+ 
+    <h2>answer to the following questions</h2>
+	<?php  
+if(isset($_post["add"])){
+	                    $pri=$_POST["pricepoint"];
+                        $prr=$_POST["pre-time"];
+                        $con=$_POST["construction"];
+                        $sty=$_POST["style"];
+                        $cus=$_POST["customizable"];
+	$sql='insert into cabinet(pricepoint,pre_time,construction,style,customizable)values("$pri","$prr","$con","$sty","$cus")';
+	
+	$conn->query($sql);
+	
+
+}
+
+
+
+?>
+
+        <form action="" method="post">
+                  <div class="container">
+				 
+                 <label>1.what is your budget?</label><br>
+                 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="pricepoint">
+				 <label class="form-check-label">(i)$</label>
+				 </div>
+				 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="pricepoint">
+				 <label class="form-check-label">(ii)$$</label> 
+				 </div>
+				 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="pricepoint">
+				 <label class="form-check-label">(iii)$$$-$$$$</label> 
+				 </div><br>
+				 
+				 <label>2.what lead time in business days is preferred?</label><br>
+                 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="pre-time">
+				 <label class="form-check-label">(i)5-10</label> </div>
+				 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="pre-time">
+				 <label class="form-check-label">(ii)20-40</label>
+				 </div>
+				 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="pre-time">
+				 <label class="form-check-label">(iii)20-45</label>
+				 </div><br>
+				 
+				 
+				 <label>3.what type of construction?</label><br>
+                 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="construction">
+				 <label class="form-check-label">(i)all-plywood</label>
+				 </div>
+				 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="construction">
+				 <label class="form-check-label">(ii)all-plywood or furniture board</label>
+				 </div>
+				 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="construction">
+				 <label class="form-check-label">(iii)all-plywood or furniture board</label>
+				 </div><br>
+				 
+				 
+				 <label>4.what style are you looking for?</label><br>
+				<div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="style">
+				 <label class="form-check-label">(i)partial overlay or full overla</label>
+				 </div>
+				 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="style">
+				 <label class="form-check-label">(ii)partial overlay , full overlay or framele</label>
+				 </div>
+				 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="style">
+				 <label class="form-check-label">(iii)partial overlay,full overlay,flush inset,beaded inset or frameless</label>
+				 </div><br>
+				 
+				 <labe>5.what level of construction?</label><br>
+				<div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="customizable">
+				 <label class="form-check-label">(i)none</label>
+				 </div>
+				 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="customizable">
+				 <label class="form-check-label">(ii)semi</label>
+				 </div>
+				 <div class="form-check">
+				 <input class="form-check-input" type="checkbox" name="customizable">
+				 <label class="form-check-label">(iii)full</label>
+				 </div><br>
+				 <div>
+
+              <button type="submit" class="btn btn-primary" name="add">Submit</button></div>
+  
+  			  
+
+</div>
+</form>
+
+
+
+
+
+
+
+<footer class="bg-dark" style="height:30px;line-height:20px;">
+   <div class="container">
+      <div class="row">
+	     <div class="col-md-12">
+		    <p class="text-center text-white" style="margin-bottom:0px;">Design & Develop By SOBUJ</p>
+	     </div>
+      </div>
+   </div>
+</footer>
+
+	
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  
+
+    
+  </body>
+</html>
+
+               
+
+
+
+
